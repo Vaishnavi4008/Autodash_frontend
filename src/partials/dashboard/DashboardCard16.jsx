f5bf60957f4f61ece2a0a0300005f1f1bebbcd6e
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2024-08-21 22:53:03 +0530

    chart fixing

--- src/partials/dashboard/DashboardCard16.jsx
+++ src/partials/dashboard/DashboardCard16.jsx
@@ -32,7 +32,7 @@ function DashboardCard06({ fetchedChartData }) {
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-gray-800 shadow-sm rounded-xl">
       <header className="px-5 py-4 border-b border-gray-100 dark:border-gray-700/60">
         <h2 className="font-semibold text-gray-800 dark:text-gray-100">
-        {fetchedChartData.chartName}
+        {fetchedChartData.chartName || ""}
         </h2>
       </header>
       {/* Chart built with Chart.js 3 */}
